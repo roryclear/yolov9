7773c8b6ebb925bc8879f99b449b6c606fe97f8b
roryclear <roryclear.rc@gmail.com>
2025-12-09 19:42:34 +0000

    restore conv

--- detect.py
+++ detect.py
@@ -74,13 +74,17 @@ class Conv(nn.Module):
     # Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)
     default_act = nn.SiLU()  # default activation
 
-    def __init__(self, c1=1, c2=1, k=1, s=1, p=None, g=1, d=1, act=True):
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
         super().__init__()
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
-    
-    def forward_fuse(self, x): return self.act(self.conv(x))
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        return self.act(self.conv(x))
 
 class ADown(nn.Module):
     def __init__(self, c1=1, c2=1):  # ch_in, ch_out, shortcut, kernels, groups, expand
