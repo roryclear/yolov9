8bd39bc539dc453f4f79cd48de01efa40b41bc2f
roryclear <roryclear.rc@gmail.com>
2025-12-11 12:52:04 +0000

    remove some torch classes

--- detect.py
+++ detect.py
@@ -74,11 +74,8 @@ def autopad(k, p=None, d=1):  # kernel, padding, dilation
     return p
 
 class Conv(nn.Module):
-    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
-        super().__init__()
-        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
-
-    def forward_fuse(self, x): return F.silu(self.conv(x))
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True): super().__init__()
+    def forward_fuse(self, x): return
 
 class tiny_Conv():
     def __init__(self):
@@ -91,20 +88,7 @@ class tiny_Conv():
       tiny_x = tiny_x.silu()
       return Tensor(tiny_x.numpy())
     
-class ADown(nn.Module):
-    def __init__(self, c1=1, c2=1):  # ch_in, ch_out, shortcut, kernels, groups, expand
-        super().__init__()
-        self.c = c2 // 2
-        self.cv1 = Conv(c1 // 2, self.c, 3, 2, 1)
-        self.cv2 = Conv(c1 // 2, self.c, 1, 1, 0)
-
-    def forward(self, x):
-        x = torch.nn.functional.avg_pool2d(x, 2, 1, 0, False, True)
-        x1,x2 = x.chunk(2, 1)
-        x1 = self.cv1(x1)
-        x2 = torch.nn.functional.max_pool2d(x2, 3, 2, 1)
-        x2 = self.cv2(x2)
-        return torch.cat((x1, x2), 1)
+class ADown(nn.Module): pass
 
 class tiny_ADown():
     def __init__(self, c1=1, c2=1): super().__init__()
@@ -125,14 +109,7 @@ class tiny_ADown():
 #                 ceil_mode=False, return_indices=False) -> Tensor | tuple[Tensor, Tensor]:
 
 
-class AConv(nn.Module):
-    def __init__(self, c1=1, c2=1):  # ch_in, ch_out, shortcut, kernels, groups, expand
-        super().__init__()
-        self.cv1 = Conv(c1, c2, 3, 2, 1)
-
-    def forward(self, x):
-        x = torch.nn.functional.avg_pool2d(x, 2, 1, 0, False, True)
-        return self.cv1(x)
+class AConv(nn.Module): pass
 
 class tiny_AConv():
     def __init__(self):  # ch_in, ch_out, shortcut, kernels, groups, expand
@@ -143,27 +120,7 @@ class tiny_AConv():
         x = tiny_Tensor.avg_pool2d(x, kernel_size=2, stride=1, padding=0, ceil_mode=False, count_include_pad=True)
         return self.cv1(x)
 
-class ELAN1(nn.Module):
-
-    def __init__(self, c1=1, c2=1, c3=1, c4=1):  # ch_in, ch_out, number, shortcut, groups, expansion
-        super().__init__()
-        self.c = c3//2
-        self.cv1 = Conv(c1, c3, 1, 1)
-        self.cv2 = Conv(c3//2, c4, 3, 1)
-        self.cv3 = Conv(c4, c4, 3, 1)
-        self.cv4 = Conv(c3+(2*c4), c2, 1, 1)
-
-    def forward(self, x):
-        y = self.cv1(x)
-        y = y.chunk(2,1)
-        y = list(y)
-        y.extend(m(y[-1]) for m in [self.cv2, self.cv3])
-        return self.cv4(torch.cat(y, 1))
-
-    def forward_split(self, x):
-        y = list(self.cv1(x).split((self.c, self.c), 1))
-        y.extend(m(y[-1]) for m in [self.cv2, self.cv3])
-        return self.cv4(torch.cat(y, 1))
+class ELAN1(nn.Module): pass
 
 class tiny_ELAN1():
     def __init__(self, c1=1, c2=1, c3=1, c4=1):  # ch_in, ch_out, number, shortcut, groups, expansion
@@ -189,17 +146,7 @@ class tiny_ELAN1():
         return self.cv4(torch.cat(y, 1))
 
 
-class RepNBottleneck(nn.Module):
-    # Standard bottleneck
-    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):  # ch_in, ch_out, shortcut, kernels, groups, expand
-        super().__init__()
-        c_ = int(c2 * e)  # hidden channels
-        self.cv1 = RepConvN(c1, c_, k[0], 1)
-        self.cv2 = Conv(c_, c2, k[1], 1, g=g)
-        self.add = shortcut and c1 == c2
-
-    def forward(self, x):
-        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+class RepNBottleneck(nn.Module): pass
     
 class tiny_RepNBottleneck():
     # Standard bottleneck
@@ -220,18 +167,7 @@ class RepConvN(nn.Module):
         exit()
 
 
-class RepNCSP(nn.Module):
-    # CSP Bottleneck with 3 convolutions
-    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
-        super().__init__()
-        c_ = int(c2 * e)  # hidden channels
-        self.cv1 = Conv(c1, c_, 1, 1)
-        self.cv2 = Conv(c1, c_, 1, 1)
-        self.cv3 = Conv(2 * c_, c2, 1)  # optional act=FReLU(c2)
-        self.m = nn.Sequential(*(RepNBottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)))
-
-    def forward(self, x):
-        return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), 1))
+class RepNCSP(nn.Module): pass
 
 class tiny_RepNCSP():
     # CSP Bottleneck with 3 convolutions
@@ -241,25 +177,7 @@ class tiny_RepNCSP():
     def __call__(self, x):
         return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), 1))
 
-class RepNCSPELAN4(nn.Module):
-    # csp-elan
-    def __init__(self, c1=1, c2=1, c3=1, c4=1, c5=1):  # ch_in, ch_out, number, shortcut, groups, expansion
-        super().__init__()
-        self.c = c3//2
-        self.cv1 = Conv(c1, c3, 1, 1)
-        self.cv2 = nn.Sequential(RepNCSP(c3//2, c4, c5), Conv(c4, c4, 3, 1))
-        self.cv3 = nn.Sequential(RepNCSP(c4, c4, c5), Conv(c4, c4, 3, 1))
-        self.cv4 = Conv(c3+(2*c4), c2, 1, 1)
-
-    def forward(self, x):
-        y = list(self.cv1(x).chunk(2, 1))
-        y.extend((m(y[-1])) for m in [self.cv2, self.cv3])
-        return self.cv4(torch.cat(y, 1))
-
-    def forward_split(self, x):
-        y = list(self.cv1(x).split((self.c, self.c), 1))
-        y.extend(m(y[-1]) for m in [self.cv2, self.cv3])
-        return self.cv4(torch.cat(y, 1))
+class RepNCSPELAN4(nn.Module): pass
 
 class tiny_RepNCSPELAN4():
     # csp-elan
@@ -276,13 +194,7 @@ class tiny_RepNCSPELAN4():
         y.extend(m(y[-1]) for m in [self.cv2, self.cv3])
         return self.cv4(torch.cat(y, 1))
 
-class SP(nn.Module):
-    def __init__(self, k=3, s=1):
-        super(SP, self).__init__()
-        self.m = nn.MaxPool2d(kernel_size=k, stride=s, padding=k // 2)
-
-    def forward(self, x):
-        return self.m(x)
+class SP(nn.Module): pass
 
 class tiny_SP():
     def __init__(self, k=3, s=1):
@@ -295,21 +207,7 @@ class tiny_SP():
         x = tiny_Tensor.max_pool2d(x, self.k, self.s, dilation=1, padding=self.k//2)
         return Tensor(x.numpy())
 
-class SPPELAN(nn.Module):
-    # spp-elan
-    def __init__(self, c1=1, c2=1, c3=1):  # ch_in, ch_out, number, shortcut, groups, expansion
-        super().__init__()
-        self.c = c3
-        self.cv1 = Conv(c1, c3, 1, 1)
-        self.cv2 = SP(5)
-        self.cv3 = SP(5)
-        self.cv4 = SP(5)
-        self.cv5 = Conv(4*c3, c2, 1, 1)
-
-    def forward(self, x):
-        y = [self.cv1(x)]
-        y.extend(m(y[-1]) for m in [self.cv2, self.cv3, self.cv4])
-        return self.cv5(torch.cat(y, 1))
+class SPPELAN(nn.Module): pass
 
 class tiny_SPPELAN():
     # spp-elan
@@ -327,14 +225,7 @@ class tiny_SPPELAN():
         y = self.cv5(y)
         return y
 
-class Concat(nn.Module):
-    # Concatenate a list of tensors along dimension
-    def __init__(self, dimension=1):
-        super().__init__()
-        self.d = dimension
-
-    def forward(self, x):
-        return torch.cat(x, self.d)
+class Concat(nn.Module): pass
 
 class tiny_Concat():
     # Concatenate a list of tensors along dimension
@@ -348,43 +239,7 @@ class tiny_Concat():
       return Tensor(y.numpy())
 
 
-class DDetect(nn.Module):
-    # YOLO Detect head for detection models
-    dynamic = False  # force grid reconstruction
-    export = False  # export mode
-    shape = None
-    anchors = torch.empty(0)  # init
-    strides = torch.empty(0)  # init
-
-    def __init__(self, nc=80, ch=(), inplace=True):  # detection layer
-        super().__init__()
-        return
-        self.nc = nc  # number of classes
-        self.nl = len(ch)  # number of detection layers
-        self.reg_max = 16
-        self.no = nc + self.reg_max * 4  # number of outputs per anchor
-        self.inplace = inplace  # use inplace ops (e.g. slice assignment)
-        self.stride = torch.zeros(self.nl)  # strides computed during build
-
-        c2, c3 = make_divisible(max((ch[0] // 4, self.reg_max * 4, 16)), 4), max((ch[0], min((self.nc * 2, 128))))  # channels
-        self.cv2 = nn.ModuleList(
-            nn.Sequential(Conv(x, c2, 3), Conv(c2, c2, 3, g=4), nn.Conv2d(c2, 4 * self.reg_max, 1, groups=4)) for x in ch)
-        self.cv3 = nn.ModuleList(
-            nn.Sequential(Conv(x, c3, 3), Conv(c3, c3, 3), nn.Conv2d(c3, self.nc, 1)) for x in ch)
-        self.dfl = DFL(self.reg_max) if self.reg_max > 1 else nn.Identity()
-
-    def __call__(self, x):
-        shape = x[0].shape  # BCHW
-        for i in range(self.nl):
-            x[i] = torch.cat((self.cv2[i](x[i]), self.cv3[i](x[i])), 1)
-        if self.dynamic or self.shape != shape:
-            self.anchors, self.strides = (x.transpose(0, 1) for x in make_anchors(x, self.stride, 0.5))
-            self.shape = shape
-
-        box, cls = torch.cat([xi.view(shape[0], self.no, -1) for xi in x], 2).split((self.reg_max * 4, self.nc), 1)
-        dbox = dist2bbox(self.dfl(box), self.anchors.unsqueeze(0), xywh=True, dim=1) * self.strides
-        y = torch.cat((dbox, cls.sigmoid()), 1)
-        return y if self.export else (y, x)
+class DDetect(nn.Module): pass
 
 class tiny_DDetect():
     # YOLO Detect head for detection models
@@ -397,20 +252,7 @@ class tiny_DDetect():
     def __init__(self, nc=80, ch=(), inplace=True):  # detection layer
         super().__init__()
         return
-        self.nc = nc  # number of classes
-        self.nl = len(ch)  # number of detection layers
-        self.reg_max = 16
-        self.no = nc + self.reg_max * 4  # number of outputs per anchor
-        self.inplace = inplace  # use inplace ops (e.g. slice assignment)
-        self.stride = torch.zeros(self.nl)  # strides computed during build
-
-        c2, c3 = make_divisible(max((ch[0] // 4, self.reg_max * 4, 16)), 4), max((ch[0], min((self.nc * 2, 128))))  # channels
-        self.cv2 = nn.ModuleList(
-            nn.Sequential(Conv(x, c2, 3), Conv(c2, c2, 3, g=4), nn.Conv2d(c2, 4 * self.reg_max, 1, groups=4)) for x in ch)
-        self.cv3 = nn.ModuleList(
-            nn.Sequential(Conv(x, c3, 3), Conv(c3, c3, 3), nn.Conv2d(c3, self.nc, 1)) for x in ch)
-        self.dfl = DFL(self.reg_max) if self.reg_max > 1 else nn.Identity()
-
+    
     def __call__(self, x):
         shape = x[0].shape  # BCHW
         for i in range(self.nl):
@@ -424,31 +266,9 @@ class tiny_DDetect():
         y = torch.cat((dbox, cls.sigmoid()), 1)
         return y if self.export else (y, x)
 
-class DFL(nn.Module):
-    # DFL module
-    def __init__(self, c1=17):
-        return
-        super().__init__()
-        self.conv = nn.Conv2d(c1, 1, 1, bias=False).requires_grad_(False)
-        self.conv.weight.data[:] = nn.Parameter(torch.arange(c1, dtype=torch.float).view(1, c1, 1, 1)) # / 120.0
-        self.c1 = c1
-        # self.bn = nn.BatchNorm2d(4)
+class DFL(nn.Module): pass
 
-    def forward(self, x):
-        b, c, a = x.shape  # batch, channels, anchors
-        return self.conv(x.view(b, 4, self.c1, a).transpose(2, 1).softmax(1)).view(b, 4, a)
-        # return self.conv(x.view(b, self.c1, 4, a).softmax(1)).view(b, 4, a)
-
-class CBLinear(nn.Module):
-    def __init__(self, c1=1, c2s=1, k=1, s=1, p=None, g=1):  # ch_in, ch_outs, kernel, stride, padding, groups
-        super(CBLinear, self).__init__()
-        return
-        self.c2s = c2s
-        self.conv = nn.Conv2d(c1, sum(c2s), k, s, autopad(k, p), groups=g, bias=True)
-
-    def forward(self, x):
-        outs = self.conv(x).split(self.c2s, dim=1)
-        return outs
+class CBLinear(nn.Module): pass
 
 class tiny_CBLinear():
     def __init__(self):  # ch_in, ch_outs, kernel, stride, padding, groups
@@ -463,17 +283,7 @@ class tiny_CBLinear():
         for i in range(len(outs)): outs[i] = Tensor(outs[i].numpy())
         return tuple(outs)
 
-class CBFuse(nn.Module):
-    def __init__(self):
-        return
-        super(CBFuse, self).__init__()
-        self.idx = idx
-
-    def forward(self, xs):
-        target_size = xs[-1].shape[2:]
-        res = [F.interpolate(x[self.idx[i]], size=target_size, mode='nearest') for i, x in enumerate(xs[:-1])]
-        out = torch.sum(torch.stack(res + xs[-1:]), dim=0)
-        return out
+class CBFuse(nn.Module): pass
 
 class tiny_CBFuse():
     def __init__(self):
@@ -529,19 +339,7 @@ def dist2bbox(distance, anchor_points, xywh=True, dim=-1):
     return torch.cat((x1y1, x2y2), dim)  # xyxy bbox
 
 
-class DFL(nn.Module):
-    # DFL module
-    def __init__(self, c1=17):
-        super().__init__()
-        self.conv = nn.Conv2d(c1, 1, 1, bias=False).requires_grad_(False)
-        self.conv.weight.data[:] = nn.Parameter(torch.arange(c1, dtype=torch.float).view(1, c1, 1, 1)) # / 120.0
-        self.c1 = c1
-        # self.bn = nn.BatchNorm2d(4)
-
-    def forward(self, x):
-        b, c, a = x.shape  # batch, channels, anchors
-        return self.conv(x.view(b, 4, self.c1, a).transpose(2, 1).softmax(1)).view(b, 4, a)
-        # return self.conv(x.view(b, self.c1, 4, a).softmax(1)).view(b, 4, a)
+class DFL(nn.Module): pass
 
 class tiny_DFL():
     # DFL module
@@ -573,11 +371,6 @@ class Silence():
         super(Silence, self).__init__()
     def __call__(self, x):    
         return x
-
-class DWConv(Conv):
-    # Depth-wise convolution
-    def __init__(self, c1, c2, k=1, s=1, d=1, act=True):  # ch_in, ch_out, kernel, stride, dilation, activation
-        super().__init__(c1, c2, k, s, g=math.gcd(c1, c2), d=d, act=act)
 
 def fuse_conv_and_bn(conv, bn):
     # Fuse Conv2d() and BatchNorm2d() layers https://tehnokv.com/posts/fusing-batchnorm-and-conv/
