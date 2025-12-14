b755cdc7fda3e9a1fa2849b43be6c3c00dc945c7
roryclear <roryclear.rc@gmail.com>
2025-12-14 21:14:39 +0000

    fix a conv init

--- detect.py
+++ detect.py
@@ -186,7 +186,7 @@ class DDetect():
         self.cv2[2] = Sequential(size=3)
         self.cv2[0][0] = Conv(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), groups=1, bias=True)
         self.cv2[0][1] = Conv(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), groups=4, bias=True)
-        self.cv2[0][2] = nn.Conv2d(16, 64, 1, 1, 1, 1, 1, True)
+        self.cv2[0][2] = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(1, 1), stride=(1, 1), padding=(0, 0), groups=4, bias=True)
         self.cv2[1][0] = Conv(96, 64, 3, 3)
         self.cv2[1][1] = Conv(16, 64, 3, 3)
         self.cv2[1][2] = nn.Conv2d(16, 64, 1, 1, 1, 1, 1, True)
@@ -732,6 +732,7 @@ if __name__ == "__main__":
       model.model[22].dfl = new_model.model[22].dfl
       model.model[22].cv2[0][0] = new_model.model[22].cv2[0][0]
       model.model[22].cv2[0][1] = new_model.model[22].cv2[0][1]
+      model.model[22].cv2[0][2] = new_model.model[22].cv2[0][2]
 
     pred = model(im)
     pred = pred[0]
